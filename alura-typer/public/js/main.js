8067fd1edb39b8ad52e73256837d3a697f9b00b9
Leonardo dos Santos Bispo <leonardosantosbispo@gmail.com>
2020-08-20 07:32:57 -0300

    aula02-finalizada

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -3,7 +3,12 @@ var tamanhoFrase = frase.split(" "); // quebra as palavras por espaço
 var numPlavras = tamanhoFrase.length; //tamanho de palavras na frase
 var tamanhoFrase = $("#tamanho-frase"); //pega o span
 tamanhoFrase.text(numPlavras); //passa o num de palavras
+var campo = $(".campo-digitacao");
+campo.on("input", function() {
+    var conteudo = campo.val();
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    $("#contador-palavras").text(qtdPalavras);
 
-
-
-console.log()
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+});
