41ce9d87d5e708f945b4546e79bf540a03a29c38
Leonardo dos Santos Bispo <leonardosantosbispo@gmail.com>
2020-08-21 07:27:05 -0300

    aula03-finalizada

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -12,3 +12,16 @@ campo.on("input", function() {
     var qtdCaracteres = conteudo.length;
     $("#contador-caracteres").text(qtdCaracteres);
 });
+
+var tempoRestante = $("#tempo-digitacao").text();
+campo.one("focus", function() {
+    var cronometroID = setInterval(() => {
+        tempoRestante--;
+        $("#tempo-digitacao").text(tempoRestante);
+        if (tempoRestante < 1) {
+            campo.attr("disabled", true);
+            clearInterval(cronometroID);
+        }
+    }, 1000);
+
+});
