290523d7af2136268923b4a795e94e78ed5cc607
Leonardo dos Santos Bispo <leonardosantosbispo@gmail.com>
2020-08-21 16:08:32 -0300

    aula04-finalizada

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,36 +1,66 @@
 var tempoInicial = $("#tempo-digitacao").text();
-var frase = $(".frase").text(); //seletor de css que retorna o elemento que eu quero
-var tamanhoFrase = frase.split(" "); // quebra as palavras por espaço
-var numPlavras = tamanhoFrase.length; //tamanho de palavras na frase
-var tamanhoFrase = $("#tamanho-frase"); //pega o span
-tamanhoFrase.text(numPlavras); //passa o num de palavras
 var campo = $(".campo-digitacao");
-campo.on("input", function() {
-    var conteudo = campo.val();
-    var qtdPalavras = conteudo.split(/\S+/).length - 1;
-    $("#contador-palavras").text(qtdPalavras);
 
-    var qtdCaracteres = conteudo.length;
-    $("#contador-caracteres").text(qtdCaracteres);
+// $(document).ready(function() {
+//     atualizarTamanhoFrase();
+//     inicializaContadores();
+//     inicializaCronometro();
+//     $("#botao-reiniciar").click(reiniciaJogo);
+// });
+
+$(function() { //É uma função ready também
+    atualizarTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $("#botao-reiniciar").click(reiniciaJogo);
 });
 
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus", function() {
-    var cronometroID = setInterval(() => {
-        tempoRestante--;
-        $("#tempo-digitacao").text(tempoRestante);
-        if (tempoRestante < 1) {
-            campo.attr("disabled", true);
-            clearInterval(cronometroID);
-        }
-    }, 1000);
+function atualizarTamanhoFrase() {
+    var frase = $(".frase").text(); //seletor de css que retorna o elemento que eu quero
+    var tamanhoFrase = frase.split(" "); // quebra as palavras por espaço
+    var numPlavras = tamanhoFrase.length; //tamanho de palavras na frase
+    var tamanhoFrase = $("#tamanho-frase"); //pega o span
+    tamanhoFrase.text(numPlavras); //passa o num de palavras
+}
 
-});
 
-$("#botao-reiniciar").click(function() {
+
+
+function inicializaContadores() {
+    campo.on("input", function() {
+        var conteudo = campo.val();
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+    });
+}
+
+
+function inicializaCronometro() {
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function() {
+        var cronometroID = setInterval(() => {
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true);
+                clearInterval(cronometroID);
+            }
+        }, 1000);
+
+    });
+}
+
+
+$("#botao-reiniciar").click(reiniciaJogo);
+
+function reiniciaJogo() {
     campo.attr("disabled", false);
     campo.val("");
     $("#contador-palavras").text("0");
     $("#contador-caracteres").text("0");
     $("#tempo-digitacao").text(tempoInicial);
-});
+    inicializaCronometro();
+}
