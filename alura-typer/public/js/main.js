4e4d61254cb326b2224008c65688937d07c9bc4a
Leonardo dos Santos Bispo <leonardosantosbispo@gmail.com>
2020-08-21 07:42:37 -0300

    aula04-jogo reiniciado

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,3 +1,4 @@
+var tempoInicial = $("#tempo-digitacao").text();
 var frase = $(".frase").text(); //seletor de css que retorna o elemento que eu quero
 var tamanhoFrase = frase.split(" "); // quebra as palavras por espaço
 var numPlavras = tamanhoFrase.length; //tamanho de palavras na frase
@@ -24,4 +25,12 @@ campo.one("focus", function() {
         }
     }, 1000);
 
+});
+
+$("#botao-reiniciar").click(function() {
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
 });
